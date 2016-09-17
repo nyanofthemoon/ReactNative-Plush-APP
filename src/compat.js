29418d490c4ef2ca1cac4227693af4bd0080ed2a
Nyan Of The Moon <nyanofthemoon@gmail.com>
2016-09-17 08:06:29 -0400

    updated dependencies

--- src/compat.js
+++ src/compat.js
@@ -1,7 +1,3 @@
 console.disableYellowBox = true
 
 window.navigator.userAgent = 'react-native'
-
-if (typeof window.Uint8Array != 'function') {
-  window.Uint8Array = require('typedarray').Uint8Array
-}
